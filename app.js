c5d4a51e2ad54e3b09325f0ac3746012572df99c
harmonicsf <harmonic.sf@gmail.com>
2016-10-31 22:06:08 +0700

    Socket add

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var io = require('socket.io')(http);
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -7,9 +8,15 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     mongoURLLabel = "";
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile('index.html');
 });
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 app.listen(port, ip, function () {
   console.log('Example app listening on port 3000!');
 });
