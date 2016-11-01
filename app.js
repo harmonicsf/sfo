112bac75aff6d324b36f4d86ddeafeddb3df4a2c
harmonicsf <harmonic.sf@gmail.com>
2016-11-01 18:07:38 +0700

    ád

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
-http.listen(port, ip, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
 });
+
 io.on('connection', function(socket){
   socket.broadcast.emit('hi');
   console.log('a user connected');
@@ -22,5 +22,8 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+});
 
+http.listen(port, ip, function () {
+  console.log('Example app listening on port 3000!');
 });
