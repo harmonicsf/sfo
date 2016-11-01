32c880c982847876b0e3b3c6b60a87a98fba3a6b
harmonicsf <harmonic.sf@gmail.com>
2016-11-01 15:57:15 +0700

    dxcfv

--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ import App from './app';
 import template from './template';
 
 const server = express();
-
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
 server.use('/assets', express.static('assets'));
 
 server.get('/', (req, res) => {
@@ -20,5 +23,5 @@ server.get('/', (req, res) => {
   }));
 });
 
-server.listen(8080);
+server.listen(port, ip);
 console.log('listening');
