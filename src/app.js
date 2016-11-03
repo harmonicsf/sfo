a1f9dd477d1a023b23e3963a603139bc69aeec85
harmonicsf <harmonic.sf@gmail.com>
2016-11-04 04:24:20 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,10 @@ import http from 'http';
 import express from 'express';
 
 let app = express();
-let app = express();
 app.server = http.createServer(app);
 app.get('/', function (req, res) {
   res.sendfile('index.html');
 });
-http.listen(port, ip, function () {
+app.server.listen(port, ip, function () {
   console.log('Example app listening on port '+port);
 });
