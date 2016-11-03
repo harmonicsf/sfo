7b9e06f20a8cd3cce2da2db3d9d86c9b7f280d1d
harmonicsf <harmonic.sf@gmail.com>
2016-11-04 04:24:47 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ import express from 'express';
 
 let app = express();
 app.server = http.createServer(app);
+
+let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+
 app.get('/', function (req, res) {
   res.sendfile('index.html');
 });
